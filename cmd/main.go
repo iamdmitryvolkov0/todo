944565dfb4325b33a84f13a0e86bdf84236c96e1
DmitryVolkov <iamdmitryvolkov@gmail.com>
2024-06-29 18:20:10 +0700

    interfaces

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"todo"
 	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err)
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	err := srv.Run("8000", handlers.InitRoutes())
+	err := srv.Run(viper.GetString("port"), handlers.InitRoutes())
 	if err != nil {
 		log.Fatalf("error starting server: %s", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../cfg")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
